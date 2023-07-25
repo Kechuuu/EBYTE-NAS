b646dea6edb2b40fe21f25ef7469e86c6af169ba
Armando Gonzalez <jesuzalez2015@gmail.com>
2023-07-25 11:58:09 -0700

    Add firebase

---
+++ EBYTE NAS/Fire_base.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace EBYTE_NAS
+{
+    partial class Fire_base
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Pallete_form = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.Table_Mac = new Bunifu.UI.WinForms.BunifuDataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Table_Mac)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Pallete_form
+            // 
+            this.Pallete_form.ButtonSpecs.FormClose.Image = global::EBYTE_NAS.Properties.Resources.Red;
+            this.Pallete_form.ButtonSpecs.FormMax.Image = global::EBYTE_NAS.Properties.Resources.Oragne;
+            this.Pallete_form.ButtonSpecs.FormMin.Image = global::EBYTE_NAS.Properties.Resources.Green;
+            this.Pallete_form.ButtonSpecs.FormRestore.Edge = ComponentFactory.Krypton.Toolkit.PaletteRelativeEdgeAlign.Far;
+            this.Pallete_form.ButtonSpecs.FormRestore.Image = global::EBYTE_NAS.Properties.Resources.Oragne;
+            this.Pallete_form.ButtonStyles.ButtonForm.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.Pallete_form.ButtonStyles.ButtonForm.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.Pallete_form.ButtonStyles.ButtonForm.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.Pallete_form.ButtonStyles.ButtonForm.StateCommon.Border.Width = 0;
+            this.Pallete_form.ButtonStyles.ButtonForm.StateNormal.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.Pallete_form.ButtonStyles.ButtonForm.StateNormal.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.Pallete_form.ButtonStyles.ButtonForm.StateNormal.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.Pallete_form.ButtonStyles.ButtonForm.StateNormal.Border.Width = 0;
+            this.Pallete_form.ButtonStyles.ButtonForm.StatePressed.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.Pallete_form.ButtonStyles.ButtonForm.StatePressed.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.Pallete_form.ButtonStyles.ButtonForm.StatePressed.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.Pallete_form.ButtonStyles.ButtonForm.StatePressed.Border.Width = 0;
+            this.Pallete_form.FormStyles.FormCommon.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.Pallete_form.FormStyles.FormCommon.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.Pallete_form.FormStyles.FormCommon.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.Pallete_form.FormStyles.FormCommon.StateCommon.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.Pallete_form.FormStyles.FormCommon.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.Pallete_form.FormStyles.FormCommon.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.None;
+            this.Pallete_form.FormStyles.FormCommon.StateCommon.Border.Rounding = 12;
+            this.Pallete_form.HeaderStyles.HeaderForm.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.Pallete_form.HeaderStyles.HeaderForm.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.Pallete_form.HeaderStyles.HeaderForm.StateCommon.ButtonEdgeInset = 10;
+            this.Pallete_form.HeaderStyles.HeaderForm.StateCommon.ButtonPadding = new System.Windows.Forms.Padding(5, -1, -1, -1);
+            // 
+            // Table_Mac
+            // 
+            this.Table_Mac.AllowCustomTheming = false;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(48)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
+            this.Table_Mac.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.Table_Mac.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Table_Mac.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.Table_Mac.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Table_Mac.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.Table_Mac.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(16)))), ((int)(((byte)(18)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI Semibold", 11.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Table_Mac.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.Table_Mac.ColumnHeadersHeight = 40;
+            this.Table_Mac.CurrentTheme.AlternatingRowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(48)))), ((int)(((byte)(52)))));
+            this.Table_Mac.CurrentTheme.AlternatingRowsStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.Table_Mac.CurrentTheme.AlternatingRowsStyle.ForeColor = System.Drawing.Color.White;
+            this.Table_Mac.CurrentTheme.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(117)))), ((int)(((byte)(119)))));
+            this.Table_Mac.CurrentTheme.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.WhiteSmoke;
+            this.Table_Mac.CurrentTheme.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(16)))), ((int)(((byte)(18)))));
+            this.Table_Mac.CurrentTheme.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(56)))), ((int)(((byte)(62)))));
+            this.Table_Mac.CurrentTheme.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(16)))), ((int)(((byte)(18)))));
+            this.Table_Mac.CurrentTheme.HeaderStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 11.75F, System.Drawing.FontStyle.Bold);
+            this.Table_Mac.CurrentTheme.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.Table_Mac.CurrentTheme.HeaderStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.Table_Mac.CurrentTheme.HeaderStyle.SelectionForeColor = System.Drawing.Color.White;
+            this.Table_Mac.CurrentTheme.Name = null;
+            this.Table_Mac.CurrentTheme.RowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            this.Table_Mac.CurrentTheme.RowsStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.Table_Mac.CurrentTheme.RowsStyle.ForeColor = System.Drawing.Color.White;
+            this.Table_Mac.CurrentTheme.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(117)))), ((int)(((byte)(119)))));
+            this.Table_Mac.CurrentTheme.RowsStyle.SelectionForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(117)))), ((int)(((byte)(119)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.Table_Mac.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Table_Mac.EnableHeadersVisualStyles = false;
+            this.Table_Mac.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(56)))), ((int)(((byte)(62)))));
+            this.Table_Mac.HeaderBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(16)))), ((int)(((byte)(18)))));
+            this.Table_Mac.HeaderBgColor = System.Drawing.Color.Empty;
+            this.Table_Mac.HeaderForeColor = System.Drawing.Color.White;
+            this.Table_Mac.Location = new System.Drawing.Point(12, 12);
+            this.Table_Mac.Name = "Table_Mac";
+            this.Table_Mac.RowHeadersVisible = false;
+            this.Table_Mac.RowHeadersWidth = 51;
+            this.Table_Mac.RowTemplate.Height = 40;
+            this.Table_Mac.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Table_Mac.Size = new System.Drawing.Size(776, 426);
+            this.Table_Mac.TabIndex = 0;
+            this.Table_Mac.Theme = Bunifu.UI.WinForms.BunifuDataGridView.PresetThemes.Dark;
+            // 
+            // Fire_base
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Table_Mac);
+            this.Name = "Fire_base";
+            this.Palette = this.Pallete_form;
+            this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Fire_base_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Table_Mac)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette Pallete_form;
+        private Bunifu.UI.WinForms.BunifuDataGridView Table_Mac;
+    }
+}
---
+++ EBYTE NAS/Fire_base.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+using Newtonsoft.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace EBYTE_NAS
+{
+
+    public partial class Fire_base : KryptonForm
+    {
+        private string MacID;
+        private string NumeroIdentificacion;
+        public Fire_base()
+        {
+            InitializeComponent();
+
+        }
+
+        public string NumeroIdentificacion1 { get => NumeroIdentificacion; set => NumeroIdentificacion = value; }
+        public string MacID1 { get => this.MacID; set => this.MacID = value; }
+
+        private void Fire_base_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string Json_data = MacID1;
+                Dictionary<string, JObject> data = JsonConvert.DeserializeObject<Dictionary<string, JObject>>(Json_data);
+
+                // Convertir el Dictionary a una lista de objetos para trabajar más fácilmente con LINQ
+                var dataList = data.Select(item => new
+                {
+                    ID = item.Key,
+                    Fecha = Convert.ToDateTime(item.Value["FECHA"].ToString()),
+                    Type = item.Value["Type"].ToString()
+                }).ToList();
+
+                // Ordenar la lista por la Fecha de manera descendente (puedes usar OrderBy para ascendente)
+                dataList = dataList.OrderByDescending(item => item.Fecha).ToList();
+
+                // Asignar la lista ordenada como fuente de datos del DataGridView
+                Table_Mac.DataSource = dataList;
+               
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier otra excepción que pueda ocurrir durante la conexión o el proceso
+                MessageBox.Show("Error general: " + ex.Message + " Error al conectar a Firebase Realtime Database");
+                
+            }
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(NumeroIdentificacion + " " + MacID);
+        }
+
+        private void Table_Mac_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+        }
+
+        private void Table_Mac_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            
+        }
+
+        private void Table_Mac_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
